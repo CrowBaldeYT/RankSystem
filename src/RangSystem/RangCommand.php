729cf640766160bce48a5166673eb3aaabe97797
CrowBaldeYT <75171427+CrowBaldeYT@users.noreply.github.com>
2021-01-12 06:31:53 +0100

    Update RangCommand.php

--- src/RangSystem/RangCommand.php
+++ src/RangSystem/RangCommand.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace RangSystem;
 
 
@@ -60,7 +61,15 @@ class RangCommand extends Command {
                         $sender->sendMessage(RangSystem::getPrefix() . "§aRänge: §r" . implode("§a,§r ", $ranks));
                         return true;
                         break;
+                    case "reload":
+                        API::getGroupConfig()->reload();
+                        foreach (RangSystem::getInstance()->getServer()->getOnlinePlayers() as $op) {
+                            API::getPlayerConfig($op)->reload();
+                            API::getUserManager()->setGroup($op, API::getUserManager()->getGroup($op));
+                        }
 
+                        $sender->sendMessage(RangSystem::getPrefix() . "§aReload erfolgreich ausgeführt.");
+                        break;
                     case "addgruppe":
                         if(isset($args[1])) {
                             if (API::getGroup($args[1])) {

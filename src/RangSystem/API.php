a97ad64bf8a4001e714fab7482811e21573ec34f
CrowBaldeYT <75171427+CrowBaldeYT@users.noreply.github.com>
2021-01-13 22:02:43 +0100

    Update API.php

--- src/RangSystem/API.php
+++ src/RangSystem/API.php
@@ -28,7 +28,7 @@ class API {
     }
 
     public static function getDataFolder() {
-        return "/root/CrowBalde/Test-Server/PocketMine-MP/plugin_data/RankSystem/";
+        return $this->getDataFolder();
     }
 
     public static function getDefaultGroup() {

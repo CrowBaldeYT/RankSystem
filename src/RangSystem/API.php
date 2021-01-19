7211f7f3276f15c6f7f0b4d6de97e85679785d0d
CrowBaldeYT <75171427+CrowBaldeYT@users.noreply.github.com>
2021-01-19 01:55:57 +0100

    Update API.php

--- src/RangSystem/API.php
+++ src/RangSystem/API.php
@@ -17,7 +17,7 @@ class API {
     }
     
     public static function getDataFolder() {
-        return self::getDataFolder();
+        return Self::$plugin->getDataFolder();
     }
 
     public static function getGroupConfig() {

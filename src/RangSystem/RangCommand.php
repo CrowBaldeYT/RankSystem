7902bc1645883b14ed9084862c7ec45e8e60e9c3
CrowBaldeYT <75171427+CrowBaldeYT@users.noreply.github.com>
2021-02-10 12:09:29 +0100

    Stable Version

--- src/RangSystem/RangCommand.php
+++ src/RangSystem/RangCommand.php
@@ -1,6 +1,8 @@
 <?php
 
+
 namespace RangSystem;
+
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -8,64 +10,63 @@ use pocketmine\Player;
 use pocketmine\utils\Config;
 use pocketmine\Server;
 use jojoe77777\FormAPI\SimpleForm;
-use jojoe77777\FormAPI\CustomForm;
 
 class RangCommand extends Command {
 
-    public function __construct(string $perm, string $name, string $description = "", string $usageMessage = null, array $aliases = [])
-    {
-        parent::__construct($name, $description, $usageMessage, $aliases);
-        $this->setPermission($perm);
-    }
+	public function __construct(string $perm, string $name, string $description = "", string $usageMessage = null, array $aliases = [])
+	{
+		parent::__construct($name, $description, $usageMessage, $aliases);
+		$this->setPermission($perm);
+	}
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if ($sender->hasPermission($this->getPermission())) {
-            if (isset($args[0])) {
-                switch ($args[0]) {
-                    case "setgroup":
-                        if (isset($args[1])) {
-                            $player = RangSystem::getInstance()->getServer()->getPlayer($args[1]);
-                            if (!file_exists(API::getDataFolder() . "players/" . $args[1] . ".yml") and !$player) {
-                                $sender->sendMessage(RangSystem::getPrefix() . "§cDieser Spieler existiert nicht oder ist Offline!");
-                                return true;
-                            } else {
-                                if (isset($args[2])) {
-                                    $cfg = new Config(API::getDataFolder() . "groups.yml", 2);
-                                    if (!$cfg->get($args[2])) {
-                                        $sender->sendMessage(RangSystem::getPrefix() . "§cDiese Gruppe existiert nicht!");
-                                        return true;
-                                    } else {
-                                        if (API::getUserManager()->getGroup($player) == $args[2]) {
-                                            $sender->sendMessage(RangSystem::getPrefix() . "§cDer Spieler hat die Gruppe bereits!");
-                                            return true;
-                                        } else {
-                                            API::getUserManager()->setGroup($player, $args[2]);
-                                            $sender->sendMessage(RangSystem::getPrefix() . "§aDu hast dem Spieler §e" . $player->getName() . " §adie Gruppe §r" . $args[2] . " §azugewiesen.");
-                                            $player->kick("§aDu wurdest der Gruppe §r" . $args[2] . " §avon §e" . $sender->getName() . " §azugewiesen." . "\n§aRejoine um dein Nametag + Rechte zu erhalten!", false);
-                                        }
-                                    }
-                                } else {
-                                    $sender->sendMessage(RangSystem::getPrefix() . "§c/rang setgroup <player> <gruppe>");
-                                }
-                            }
-                        } else {
-                            $sender->sendMessage(RangSystem::getPrefix() . "§c/rang setgroup <player> <gruppe>");
-                        }
-                        break;
-                    case "groups":
-                        if(!$sender instanceof Player){
-                            $ranks = [];
-                            foreach (API::getAllGroups() as $group) {
-                                $ranks[] = $group;
-                            }
+	public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+		if ($sender->hasPermission($this->getPermission())) {
+			if (isset($args[0])) {
+				switch ($args[0]) {
+					case "setgroup":
+						if (isset($args[1])) {
+							$player = RangSystem::getInstance()->getServer()->getPlayer($args[1]);
+							if (!file_exists(API::getDataFolder() . "players/" . $args[1] . ".yml") and !$player) {
+								$sender->sendMessage(RangSystem::getPrefix() . "§cDieser Spieler existiert nicht oder ist Offline!");
+								return true;
+							} else {
+								if (isset($args[2])) {
+									$cfg = new Config(API::getDataFolder() . "groups.yml", 2);
+									if (!$cfg->get($args[2])) {
+										$sender->sendMessage(RangSystem::getPrefix() . "§cDiese Gruppe existiert nicht!");
+										return true;
+									} else {
+										if (API::getUserManager()->getGroup($player) == $args[2]) {
+											$sender->sendMessage(RangSystem::getPrefix() . "§cDer Spieler hat die Gruppe bereits!");
+											return true;
+										} else {
+											API::getUserManager()->setGroup($player, $args[2]);
+											$sender->sendMessage(RangSystem::getPrefix() . "§aDu hast dem Spieler §e" . $player->getName() . " §adie Gruppe §r" . $args[2] . " §azugewiesen.");
+											$player->kick("§aDu wurdest der Gruppe §r" . $args[2] . " §avon §e" . $sender->getName() . " §azugewiesen." . "\n§aRejoine um dein Nametag + Rechte zu erhalten!", false);
+										}
+									}
+								} else {
+									$sender->sendMessage(RangSystem::getPrefix() . "§c/rang setgruppe <player> <gruppe>");
+								}
+							}
+						} else {
+							$sender->sendMessage(RangSystem::getPrefix() . "§c/rang setgruppe <player> <gruppe>");
+						}
+						break;
+					case "groups":
+						if(!$sender instanceof Player){
+							$ranks = [];
+							foreach (API::getAllGroups() as $group) {
+								$ranks[] = $group;
+							}
 
-                            $sender->sendMessage(RangSystem::getPrefix() . "§aRänge: §r" . implode("§a,§r ", $ranks));
-                        } else {
-                            $this->groupsUI($sender);
-                        }
-                        break;
+							$sender->sendMessage(RangSystem::getPrefix() . "§aRänge: §r" . implode("§a,§r ", $ranks));
+						} else {
+							$this->groupsUI($sender);
+						}
+						break;
 
-                    case "reload":
+					case "reload":
 						if(!$sender instanceof Player){
 							API::getGroupConfig()->reload();
 							foreach (RangSystem::getInstance()->getServer()->getOnlinePlayers() as $op) {
@@ -77,82 +78,72 @@ class RangCommand extends Command {
 						} else {
 							$this->reloadUI($sender);
 						}
-                        break;
+						break;
 
-                    case "addgroup":
-                        if(isset($args[1])) {
-                            if (API::getGroup($args[1])) {
-                                $sender->sendMessage(RangSystem::getPrefix() . "§cDiese Gruppe existiert bereits!");
-                                return true;
-                            } else {
-                                API::addGroup($args[1]);
-                                $sender->sendMessage(RangSystem::getPrefix() . "§aDie Gruppe §r" . $args[1] . " §awurde erfolgreich erstellt.");
-                            }
-                        } else {
-                            $sender->sendMessage(RangSystem::getPrefix() . "§c/rang addgroup <name>");
-                        }
-                      }
-                      break;
-                      
-                    case "addgroupui":
-                    if(!$sender instanceof Player){
-                      $sender->sendMessage(RangSystem::getPrefix() . "§cDu musst denn Command §7/addgroup <name>§c eingeben!");
-                    } else {
-                    $this->addGroupUI($sender);
-                    }
-                    break;
+					case "addgroup":
+						if(isset($args[1])) {
+							if (API::getGroup($args[1])) {
+								$sender->sendMessage(RangSystem::getPrefix() . "§cDiese Gruppe existiert bereits!");
+								return true;
+							} else {
+								API::addGroup($args[1]);
+								$sender->sendMessage(RangSystem::getPrefix() . "§aDie Gruppe §r" . $args[1] . " §awurde erfolgreich erstellt.");
+							}
+						} else {
+							$sender->sendMessage(RangSystem::getPrefix() . "§c/rang addgroup <name>");
+						}
+						break;
 
-                    case "removegroup":
-                        if(isset($args[1])) {
-                            if (!API::getGroup($args[1])) {
-                                $sender->sendMessage(RangSystem::getPrefix() . "§cDiese Gruppe existiert nicht!");
-                                return true;
-                            } else {
-                                API::removeGroup($args[1]);
-                                $sender->sendMessage(RangSystem::getPrefix() . "§aDie Gruppe §r" . $args[1] . " §awurde erfolgreich gelöscht.");
-                            }
-                        } else {
-                            $sender->sendMessage(RangSystem::getPrefix() . "§c/rang removegroup <name>");
-                        }
-                }
-            } else {
-                $sender->sendMessage(RangSystem::getPrefix() . "§c/rang setgroup <player> <gruppe>");
-                $sender->sendMessage(RangSystem::getPrefix() . "§c/rang groups");
-                $sender->sendMessage(RangSystem::getPrefix() . "§c/rang addgroup <name>");
-                $sender->sendMessage(RangSystem::getPrefix() . "§c/rang addgroupui §7(For Onlineplayer)");
-                $sender->sendMessage(RangSystem::getPrefix() . "§c/rang removegroup <gruppe>");
-                $sender->sendMessage(RangSystem::getPrefix() . "§c/rang reload");
+					case "removegroup":
+						if(isset($args[1])) {
+							if (!API::getGroup($args[1])) {
+								$sender->sendMessage(RangSystem::getPrefix() . "§cDiese Gruppe existiert nicht!");
+								return true;
+							} else {
+								API::removeGroup($args[1]);
+								$sender->sendMessage(RangSystem::getPrefix() . "§aDie Gruppe §r" . $args[1] . " §awurde erfolgreich gelöscht.");
+							}
+						} else {
+							$sender->sendMessage(RangSystem::getPrefix() . "§c/rang removegroup <name>");
+						}
+				}
+			} else {
+				$sender->sendMessage(RangSystem::getPrefix() . "§c/rang setgroup <player> <gruppe>");
+				$sender->sendMessage(RangSystem::getPrefix() . "§c/rang groups");
+				$sender->sendMessage(RangSystem::getPrefix() . "§c/rang addgroup <name>");
+				$sender->sendMessage(RangSystem::getPrefix() . "§c/rang removegroup <gruppe>");
+				$sender->sendMessage(RangSystem::getPrefix() . "§c/rang reload");
 
-            }
-        } else {
-            $sender->sendMessage(RangSystem::getPrefix() . "§cDafür hast du keine Rechte!");
-        }
-        return true;
-    }
+			}
+		} else {
+			$sender->sendMessage(RangSystem::getPrefix() . "§cDafür hast du keine Rechte!");
+		}
+		return true;
+	}
 
-    public function groupsUI(Player $player){
-        $form = new SimpleForm(function (Player $player, int $data = null) {
-            $result = $data;
-            if($result === null){
-                return true;
-            }
-            switch($result) {
+	public function groupsUI(Player $player){
+		$form = new SimpleForm(function (Player $player, int $data = null) {
+			$result = $data;
+			if($result === null){
+				return true;
+			}
+			switch($result) {
 				case 0:
-                    $ranks = [];
-                    foreach (API::getAllGroups() as $group) {
-                        $ranks[] = $group;
-                    }
+					$ranks = [];
+					foreach (API::getAllGroups() as $group) {
+						$ranks[] = $group;
+					}
 
-                    $player->sendMessage(RangSystem::getPrefix() . "§aRänge: §r" . implode("§a,§r ", $ranks));
-                    break;
-            }
-        });
-        $form->setTitle(RangSystem::getPrefix());
-        $form->setContent("§aZeige dir alle Ränge an.");
-        $form->addButton("§7Senden");
-        $form->sendToPlayer($player);
-        return $form;
-    }
+					$player->sendMessage(RangSystem::getPrefix() . "§aRänge: §r" . implode("§a,§r ", $ranks));
+					break;
+			}
+		});
+		$form->setTitle(RangSystem::getPrefix());
+		$form->setContent("§aZeige dir alle Ränge an.");
+		$form->addButton("§7Senden");
+		$form->sendToPlayer($player);
+		return $form;
+	}
 
 	public function reloadUI(Player $player){
 		$form = new SimpleForm(function (Player $player, int $data = null) {
@@ -177,27 +168,5 @@ class RangCommand extends Command {
 		$form->addButton("§7Senden");
 		$form->sendToPlayer($player);
 		return $form;
-	}
-	
-	public function addGroupUI(Player $player){
-	  $form = new CustomForm(function(Player $player, array $data = null){
-	    
-	  if($data === null){
-	    return true;
-	  }
-	  
-	  if (API::getGroup($data[1])) {
-          $player->sendMessage(RangSystem::getPrefix() . "§cDiese Gruppe existiert bereits!");
-        } else {
-         API::addGroup($data[1]);
-          $player->sendMessage(RangSystem::getPrefix() . "§aDie Gruppe §r" . $data[1] . " §awurde erfolgreich erstellt.");
-    }
-    
-	});
-	$form->setTitle(RangSystem::getPrefix());
-	$form->addLabel("§7Hinzufüge einen Rang");
-	$form->addInput("§fRang§7-§fName§7:", "§7Schreibe einen namen für denn Rang");
-	$form->sendToPlayer($player);
-	return true;
 	}
 }
